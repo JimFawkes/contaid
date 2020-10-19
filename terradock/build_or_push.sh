88606ff4261a261aaaa1a9dde3d427f2b4547e44
Jim Fawkes <JimFawkes@users.noreply.github.com>
2020-10-19 23:14:43 +0200

    Seperate Workflow Paths to Build or Build-and-Push

--- terradock/build_or_push.sh
+++ terradock/build_or_push.sh
@@ -5,6 +5,7 @@ VERSIONS=$(awk 'NR>1' $VERSIONS_FILE_PATH)
 IMAGE_NAME=jimfawkes/terradock
 
 build () {
+	echo "Build images for every version tuple in $VERSIONS_FILE_PATH"
 	image_tags=()
     while IFS=, read -r terraform terragrunt; do
     	tag=tf_$terraform-tg_$terragrunt
@@ -15,15 +16,21 @@ build () {
 }
 
 push () {
+	echo "Push images for IMAGE_TAGS=$IMAGE_TAGS"
 	for tag in ${IMAGE_TAGS}; do
     	docker push $IMAGE_NAME:${tag}
 	done
 }
 
-echo "Build and Push terradock images"
 
 if [[ "$1" == "build" ]]; then
+	echo "Build images"
 	build
 elif [[ "$1" == "push" ]]; then
+	echo "Push images"
+	push
+elif [[ "$1" == "build-and-push" ]]; then
+	echo "Build and Push terradock images"
+	build
 	push
 fi
